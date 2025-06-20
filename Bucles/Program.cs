152dbad81bbe87f0f13f37e023f5ac3a195670c7
Matias Soto <matias.ds.soto@gmail.com>
2025-06-19 23:54:28 -0300

    Primer commit con carpeta Bucles incluida

---
+++ Bucles/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Bucles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bucles");
+            Console.WriteLine();
+
+            //While
+            int Contador = 0; //Probar inicializando Contador en 10
+            Console.WriteLine("while");
+            Console.WriteLine();
+            while (Contador < 10)
+            {
+                Console.WriteLine("contador = {0}", Contador);
+                Contador = Contador + 1;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Valor final: {0}", Contador);
+
+            Console.WriteLine();
+            Console.WriteLine("Pulse una tecla para continuar...");
+            Console.ReadKey();
+            Console.Clear();
+
+            //Do While
+            Console.WriteLine("do while");
+            Console.WriteLine();
+            Contador = 0; //probar inicializando Contador en 10
+            do
+            {
+                Console.WriteLine("contador = {0}", Contador);
+                Contador = Contador + 1;
+
+            }
+            while (Contador < 10);
+            {
+                Console.WriteLine();
+                Console.WriteLine("Valor final: {0}", Contador);
+
+                Console.WriteLine();
+                Console.WriteLine("Pulse una tecla para continuar...");
+                Console.ReadKey();
+                Console.Clear();
+
+                //for
+                Console.WriteLine("for");
+                Console.WriteLine();
+                for (int Var = 1; Var <= 10; Var++)
+                {
+                    Console.WriteLine("Var = {0}", Var);
+                }
+
+                // otro ejemplo
+                for (int Var = 100; Var >= 10; Var = Var - 10)
+                {
+                    Console.WriteLine();
+                    Console.Write("Pulse una tecla para continuar...");
+                    Console.ReadKey();
+
+                    Console.Clear();
+                }
+            }
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace Condicionales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Var1 = 10;
+            int Var2 = 20;
+            int Var3 = 3;
+
+            Console.WriteLine("Condicionales");
+            Console.WriteLine();
+
+            Console.WriteLine("Var1 = {0}", Var1);
+            Console.WriteLine("Var2 = {0}", Var2);
+            Console.WriteLine("Var3 = {0}", Var3);
+            Console.WriteLine();
+
+            //If simple
+            if (Var1 == Var2)
+            {
+                Console.WriteLine("El valor de ambas variables es el mismo.");
+            }
+
+            //If con else
+            if (Var1 == Var2)
+            {
+                Console.WriteLine("El valor de ambas variables es el mismo");
+            }
+            else
+            {
+                Console.WriteLine("El valor de ambas variables es distinto");
+            }
+
+            Console.WriteLine();
+
+            //If anidado
+            if (Var1 == Var2)
+            {
+                Console.WriteLine("El valor de ambas variables es el mismo.");
+                Console.WriteLine();
+            }
+            else
+            {
+                if (Var1 > Var2)
+                {
+                    Console.WriteLine("Var1 > Var2.");
+                }
+                else
+                {
+                    Console.WriteLine("Var1 < Var2.");
+                }
+                Console.WriteLine();
+            }
+
+            switch (Var3)
+            {
+                case 1:
+                    Console.WriteLine("var3 = primavera.");
+                    break;
+
+                case 2:
+                    Console.WriteLine("var3 = verano.");
+                    break;
+
+                case 3:
+                    Console.WriteLine("var3 = otoño.");
+                    break;
+
+                case 4:
+                    Console.WriteLine("var3 = invierno.");
+                    break;
+                default: //si no es ninguno de los dos indicado arriba
+                    Console.WriteLine("var3 fuera de rango.");
+                    break;
+            }
+
+            Console.WriteLine();
+
+            //switch con rangos
+            switch (Var3)
+            {
+                case 1:
+                case 2:
+                case 3:
+                    Console.WriteLine("var3 1, 2, o 3");
+                    break;
+                case 4:
+                    Console.WriteLine("var3 4");
+                    break;
+
+                default:
+                    Console.WriteLine("var3 fuera de rango.");
+                    break;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Pulse una tecla para terminar...");
+            Console.ReadKey();
+
+            
+        } 
+    }
+}
